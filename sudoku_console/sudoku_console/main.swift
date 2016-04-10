a30e8925240f2d1b83e0f626f514d2a0d98b16b6
leonzyz <liusha@liushadeMacBook-Air.local>
2016-04-10 11:37:37 +0800

    sudoku project initial version

---
+++ sudoku_console/sudoku_console/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  sudoku_console
+//
+//  Created by 刘莎 on 16/3/23.
+//  Copyright © 2016年 刘莎. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var sudoku_table=[[Int]]();
+for row in 1...9{
+	var this_row=[Int]();
+	for col in 1...9{
+		let tmp=arc4random()%9+1;
+		print("\(tmp)\t",terminator:"");
+		this_row.append(Int(tmp));
+	}
+	sudoku_table.append(this_row);
+}
+
+
+
